2e0c6252df76be0a71463da021986496efa7fcda
Astha Sharma <a@astha.me>
2016-02-02 09:19:58 +0530

    Added buttons for session length

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 function updateTime(){
 	var circle = document.getElementById("time");
 	var d = new Date();
-	var h = (d.getHours() < 10?'0':d.getHours());
-	var m = (d.getMinutes() < 10?'0':d.getMinutes());
-	var s = d.getSeconds();
+	var h = (d.getHours() < 10?'0'+d.getHours():d.getHours());
+	var m = (d.getMinutes() < 10?'0'+d.getMinutes():d.getMinutes());
+	var s = (d.getSeconds() < 10?'0'+d.getSeconds():d.getSeconds());
 	var date = h+':' + m + ':' + s;
 	circle.innerHTML = date;
 }
 
 setInterval(updateTime, 1000);
+
+$(document).ready(function(){
+	var counter = 0;
+	$("#plus").click(function(){
+		counter++;
+		$("p").html(counter);
+	});
+	$("#minus").click(function(){
+		if(counter > 0){
+			counter--;
+		}
+		
+		$("p").html(counter);
+	});
+	
+});
+
